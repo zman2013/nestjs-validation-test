b16401e8fda18659ece308260ed4ccdb4f2cda99
zman <manzhiyuan@lixiang.com>
2020-12-23 23:31:45 +0800

    修改注释

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 加上默认转换类型
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
